9f93571b61e0b7fe4ad63adce207cf566d767724
VinhTruong <vinh.truong@vietlinkads.com>
2023-06-06 22:47:33 +0700

    navigate back with a result

--- app/src/main/java/com/example/philliplackner/MainActivity.kt
+++ app/src/main/java/com/example/philliplackner/MainActivity.kt
@@ -3,44 +3,63 @@ package com.example.philliplackner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.philliplackner.ui.theme.PhillipLacknerTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PhillipLacknerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "screen1") {
+                    composable("screen1") { entry ->
+                        val text = entry.savedStateHandle.get<String>("my_text")
+                        Column(modifier = Modifier.fillMaxSize()) {
+                            text?.let {
+                                Text(text = text)
+                            }
+                            Button(onClick = { navController.navigate("screen2") }) {
+                                Text(text = "Go to Screen 2")
+                            }
+                        }
+                    }
+                    composable("screen2") {
+                        Column(modifier = Modifier.fillMaxSize()) {
+                            var text by remember { mutableStateOf("") }
+                            OutlinedTextField(
+                                value = text,
+                                onValueChange = { text = it },
+                                modifier = Modifier.width(300.dp)
+                            )
+                            Button(onClick = {
+                                navController.previousBackStackEntry?.savedStateHandle?.set(
+                                    "my_text", text
+                                )
+                                navController.popBackStack()
+                            }) {
+                                Text(text = "Aplly")
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhillipLacknerTheme {
-        Greeting("Android")
     }
 }
