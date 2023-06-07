6250510ade67925aa7f18b5e785609f5d5cdc938
VinhTruong <vinh.truong@vietlinkads.com>
2023-06-07 21:39:13 +0700

    nested navigation graphs

--- app/src/main/java/com/example/philliplackner/MainActivity.kt
+++ app/src/main/java/com/example/philliplackner/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.philliplackner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
 import com.example.philliplackner.ui.theme.PhillipLacknerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +21,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PhillipLacknerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("about") {
+
+                    }
+                    navigation(startDestination = "login", route = "auth") {
+                        composable("login") {
+//                            val viewModel = it.sharedViewModel<SampleViewModel>(navController = navController)
+                            Button(onClick = {
+                                navController.navigate("calendar") {
+                                    popUpTo("auth") { inclusive = true }
+                                }
+                            }) {
+
+                            }
+                        }
+                        composable("register") {
+//                            val viewModel = it.sharedViewModel<SampleViewModel>(navController = navController)
+
+                        }
+                        composable("forgot_password") {
+//                            val viewModel = it.sharedViewModel<SampleViewModel>(navController = navController)
+
+                        }
+                    }
+                    navigation(startDestination = "calendar_overview", route = "calendar") {
+                        composable("calendar_overview") {
+
+                        }
+                        composable("calendar_entry") {
+
+                        }
+                    }
                 }
             }
         }
@@ -30,17 +61,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhillipLacknerTheme {
-        Greeting("Android")
+inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(navController: NavController): T {
+    val navGraphRoute = destination.parent?.route ?: return viewModel()
+    val parentEntry = remember(this) {
+        navController.getBackStackEntry(navGraphRoute)
     }
+    return viewModel(parentEntry)
 }
